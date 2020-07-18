558afe6d3d6158dacf7ebd4008d1eadd91a222d9
Luigi <luigitosin@gmail.com>
2020-07-17 23:46:44 -0300

    Inicialização do repositório

---
+++ src/main/java/com/br/lughtech/GranaMailApplication.java
@@ -0,0 +1,13 @@
+package com.br.lughtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranaMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranaMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/lughtech/GranaMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lughtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranaMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
